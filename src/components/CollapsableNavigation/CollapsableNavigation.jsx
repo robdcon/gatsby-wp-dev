185766679793f985ac2a5e151a8328a9dc0057a0
robdcon <rob@innerspace.digital>
2021-07-06 17:12:25 +0100

    adds basic nav bar with collape function

--- src/components/CollapsableNavigation/CollapsableNavigation.jsx
+++ src/components/CollapsableNavigation/CollapsableNavigation.jsx
@@ -1,26 +1,36 @@
 import React, {useRef, useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
-import { NavWrapper, Nav, NavListItem, NavLink } from './CollapsableNavigation.styles';
+import { NavWrapper, Nav, NavListItem, NavLink, MenuToggle } from './CollapsableNavigation.styles';
 
 const CollapsableNavigation = ({pages}) => {
- console.log('pages: ', pages)
+  const [navOpen, setNavOpen] = useState(false);
+
+  const handleNav = () => {
+    const isOpen = navOpen;
+    setNavOpen(!isOpen);
+    console.log(navOpen);
+  }
+
   return (
-  <NavWrapper className="CollapsableNavigationWrapper">
-    <Nav>
-    {
-      pages && pages.map(({node}) => {
-        console.log(node)
-        return(
-          <NavListItem>
-            <NavLink key={node.slug} to={node.link}>
-              {node.title}
-            </NavLink>
-          </NavListItem>
-        )
-      })
-    }
-    </Nav>
-  </NavWrapper>
+  <>
+    <MenuToggle onClick={handleNav} className={`nav-open-${navOpen}`} />
+    <NavWrapper className={`CollapsableNavigationWrapper nav-open-${navOpen}`}>
+      <Nav>
+      {
+        pages && pages.map(({node}) => {
+          console.log(node)
+          return(
+            <NavListItem>
+              <NavLink key={node.slug} to={node.link}>
+                {node.title}
+              </NavLink>
+            </NavListItem>
+          )
+        })
+      }
+      </Nav>
+    </NavWrapper>
+  </>
   )
 };
 
--- src/components/CollapsableNavigation/CollapsableNavigation.styles.js
+++ src/components/CollapsableNavigation/CollapsableNavigation.styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import MenuIcon from '@material-ui/icons/Menu'
 import { media } from '../../utils/media';
 import { Link } from "gatsby"
 
@@ -12,6 +13,10 @@ export const NavWrapper = styled.nav`
     justify-content: center;
     background-color: #000;
     min-height: 100vh;
+
+    &.nav-open-false {
+        display: none;
+    }
 
     ${media.desktop`
         min-height: 80px;
@@ -48,4 +53,23 @@ export const NavListItem = styled.li`
 export const NavLink = styled(Link)`
     color: #fff;
     text-decoration: none;
+`;
+
+export const MenuToggle = styled(MenuIcon)`
+    position: fixed;
+    right: 24px;
+    top: 24px;
+    z-index: 999;
+    color: #fff;
+
+    &.nav-open-false {
+        color: #000;
+    }
+
+    ${media.desktop`
+        display: none;
+    `}
+
+
+
 `;
