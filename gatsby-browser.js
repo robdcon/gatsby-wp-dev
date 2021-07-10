2b8f423aaad3b059f9d823a34b02a2a236ca07c6
robdcon <rob@innerspace.digital>
2021-07-10 13:38:12 +0100

    add normalise and base to global styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,2 @@
-// custom typefaces
-// import "typeface-montserrat"
-// import "typeface-merriweather"
-// normalize CSS across browsers
 import "@fontsource/montserrat/900.css";
 import "@fontsource/montserrat/600.css";
-import "./src/normalize.css"
-// custom CSS styles
-import "./src/style.css"
