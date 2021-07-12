206055a8fed3d35fd59a90e6ca2110794d99aa6f
robdcon <rob@innerspace.digital>
2021-07-12 08:36:41 +0100

    set header styles for logo

--- src/components/organisms/Header/Header.styles.js
+++ src/components/organisms/Header/Header.styles.js
@@ -3,10 +3,12 @@ import { display } from '../../../utils/mixins';
 
 export const StyledHeader = styled.header`
     ${display};
+    justify-content: space-between;
     width: 100%;
     padding: 16px 24px;
     position: fixed;
     top: 0;
     left: 0;
+    background-color: #000;
     z-index: var(--stack-level-1);
 `;
