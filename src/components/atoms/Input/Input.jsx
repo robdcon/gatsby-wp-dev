c589aefc3d294e1fdeb5acb22e64e4da9251578c
robdcon <rob@innerspace.digital>
2021-07-17 14:28:46 +0100

    add input to atoms

---
+++ src/components/atoms/Input/Input.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyledInput } from './Input.styles';
+
+const Input = (props) => (
+  <StyledInput {...props} className="InputWrapper" />
+);
+
+
+export default Input;
---
+++ src/components/atoms/Input/Input.styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledInput = styled.div`
+    border-radius: 6px;
+    width:100%;
+    padding: 16px;
+    margin-bottom: 8px;
+    background-color: #fff;
+`;
