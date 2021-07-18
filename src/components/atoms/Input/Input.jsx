33657ce3844160bfad4ca816e4e28ad9b9dfaf41
robdcon <rob@innerspace.digital>
2021-07-18 23:03:16 +0100

    style input

--- src/components/atoms/Input/Input.jsx
+++ src/components/atoms/Input/Input.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyledInput } from './Input.styles';
 
 const Input = (props) => (
-  <StyledInput {...props} className="InputWrapper" />
+  <StyledInput {...props} className="input" />
 );
 
 
