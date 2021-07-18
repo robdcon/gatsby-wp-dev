35226a7c7eba943f99395758530466b93cfda96a
robdcon <rob@innerspace.digital>
2021-07-18 23:04:03 +0100

    add textarea to atoms

---
+++ src/components/atoms/Textarea/Textarea.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyledTextarea } from './Textarea.styles';
+
+const Textarea = (props) => (
+  <StyledTextarea {...props} className="textarea" />
+);
+
+export default Textarea;
