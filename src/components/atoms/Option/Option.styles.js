f18d053b297ea8c1ed2dc38df7e4c171e44aa157
robdcon <rob@innerspace.digital>
2021-07-18 23:03:47 +0100

    fix option output

--- src/components/atoms/Option/Option.styles.js
+++ src/components/atoms/Option/Option.styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const StyleOption = styled.div`
+export const StyledOption = styled.option`
     border-radius: 6px;
     padding: 16px;
     background-color: #fff;
