51982a4eaed0e5117103b872fdea44c7130a71d7
robdcon <rob@innerspace.digital>
2021-07-18 23:07:00 +0100

    add theme styles

--- src/themes/Theme.js
+++ src/themes/Theme.js
@@ -42,7 +42,6 @@ const Theme = {
         lightText: colors.darkText,
         darkText: colors.lightText
     },
-
     primaryFont: fonts.primaryFont,
     sectionPadding: "32px 24px 48px 24px",
     cardPadding: "16px 16px 24px 16px",
@@ -50,11 +49,8 @@ const Theme = {
     cardShadow: "0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)",
     maxTextWidth: "800px",
     subheadingLineHeight: "1.3",
-
-    horizontalNavStyles: `
-        display: flex;
-        flex-direction: column;
-    `
+    borderStyle: "2px solid #ddd",
+    horizontalNavStyles: "display: flex; flex-direction: column"
 }
 
 
