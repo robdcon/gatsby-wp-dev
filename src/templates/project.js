7aaa869753e4805c2e9813a6a103312516ad725f
robdcon <rob@innerspace.digital>
2021-07-20 22:51:34 +0100

    remove wordpress block styles

--- src/templates/project.js
+++ src/templates/project.js
@@ -4,8 +4,8 @@ import { GatsbyImage } from "gatsby-plugin-image";
 import parse from "html-react-parser"
 
 // We're using Gutenberg so we need the block styles
-import "@wordpress/block-library/build-style/style.css"
-import "@wordpress/block-library/build-style/theme.css"
+// import "@wordpress/block-library/build-style/style.css"
+// import "@wordpress/block-library/build-style/theme.css"
 
 import Bio from "../components/bio"
 import Layout from "./layout"
