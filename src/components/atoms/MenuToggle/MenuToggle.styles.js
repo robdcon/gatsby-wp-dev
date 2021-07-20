84d928497e7e407b025bae67ecd18130601c1764
robdcon <rob@innerspace.digital>
2021-07-20 10:14:13 +0100

    remove material-ui ref fro pef check

--- src/components/atoms/MenuToggle/MenuToggle.styles.js
+++ src/components/atoms/MenuToggle/MenuToggle.styles.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
-import MenuIcon from '@material-ui/icons/Menu';
+// import MenuIcon from '@material-ui/icons/Menu';
 import { media } from '../../../utils/media';
 
 
-export const MenuButton = styled(MenuIcon)`
+export const MenuButton = styled.i`
     right: 24px;
     top: 24px;
     z-index: var(--stack-level-1);
