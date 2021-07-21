690f17f6295116f73717f60d60d2351f5ce2fa89
robdcon <rob@innerspace.digital>
2021-07-21 18:57:49 +0100

    add menu toggle to header

--- src/components/atoms/MenuToggle/MenuToggle.styles.js
+++ src/components/atoms/MenuToggle/MenuToggle.styles.js
@@ -1,8 +1,9 @@
+import { MenuItem } from '@material-ui/core';
 import styled from 'styled-components';
 import { media } from '../../../utils/media';
 
 
-export const MenuButton = styled.i`
+export const MenuButton = styled(MenuItem)`
     right: 24px;
     top: 24px;
     z-index: var(--stack-level-1);
