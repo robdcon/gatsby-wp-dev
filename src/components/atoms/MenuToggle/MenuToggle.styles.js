abbb96ed8eda4497f69b7aae79d294d8a3897c6a
robdcon <rob@innerspace.digital>
2021-07-21 20:57:52 +0100

    add close button

--- src/components/atoms/MenuToggle/MenuToggle.styles.js
+++ src/components/atoms/MenuToggle/MenuToggle.styles.js
@@ -1,7 +1,7 @@
 import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
 import styled from 'styled-components';
 import { media } from '../../../utils/media';
-
 
 export const MenuButton = styled(MenuIcon)`
     right: 24px;
@@ -9,9 +9,16 @@ export const MenuButton = styled(MenuIcon)`
     z-index: var(--stack-level-1);
     color: ${({theme}) => theme.primaryColor};
 
-    &.nav-open-false {
-        color: ${({theme}) => theme.primaryColor};
-    }
+    ${media.desktop`
+        display: none;
+    `}
+`;
+
+export const CloseButton = styled(CloseIcon)`
+    right: 24px;
+    top: 24px;
+    z-index: var(--stack-level-1);
+    color: ${({theme}) => theme.primaryColor};
 
     ${media.desktop`
         display: none;
